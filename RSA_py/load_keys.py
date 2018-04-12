47545191e795828dc79829b3908706296161b38f
r3ddy97 <venkyreddy097@gmail.com>
2018-04-13 01:45:01 +0530

    python2 compatible

--- RSA_py/load_keys.py
+++ RSA_py/load_keys.py
@@ -6,10 +6,9 @@ import os
 def load_key(key_file):
     try:
         file_path = os.path.abspath(key_file)
-        if os.path.isfile(file_path):
-            with open(file_path, "r+") as pkey:
-                key_params = pkey.read()
-            return key_params
+        with open(file_path, "r+") as pkey:
+            key_params = pkey.read()
+        return key_params
     except TypeError:
         pass
     return None
