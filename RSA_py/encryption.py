397355a3db44523067c0a373fee0a135e8e297b1
r3ddy97 <venkyreddy097@gmail.com>
2018-04-13 01:45:24 +0530

    sage module import

--- RSA_py/encryption.py
+++ RSA_py/encryption.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python2
 
-import os
+# import os
 # import sympy
 from sage import all as sage_all
 from load_keys import load_key
 
 
 def encrypt_message(message, public_key):
-    key_params = load_key(public_key)
     try:
-        public_exponent, modulus = key_params or public_key
-    except TypeError:
-        print("wrong args")
-        os.sys.exit()
-    cipher_text = " ".join([str(sage_all.powermod(ord(char), public_exponent, modulus)) for char in message])
+        public_exponent, modulus = load_key(public_key)
+    except:
+        public_exponent, modulus = public_key
+    cipher_text = " ".join([str(sage_all.power_mod(ord(char), public_exponent, modulus)) for char in message])
     return cipher_text
     print("Entcrypted message: \n\n", cipher_text)
