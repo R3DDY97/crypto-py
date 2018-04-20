c8901bf7dcbcd08c5bba9d91664c2a0646c632e3
r3ddy97 <venkyreddy097@gmail.com>
2018-04-20 14:19:34 +0530

    change egs

--- AES_py/AES_demo/aes_demo.py
+++ AES_py/AES_demo/aes_demo.py
@@ -7,158 +7,132 @@ from aes_constants import *
 
 class Rijndael(object):
 
-    def __init__(self, key, block_size=16):
-        self.block_size = block_size
+    def __init__(self, key):
 
-        ROUNDS = 10
-        BC = int(block_size / 4)
         # encryption round keys
-        Ke = [[0] * BC for i in range(ROUNDS + 1)]
+        Ke = [[0] * 4 for i in range(11)]
         # decryption round keys
-        Kd = [[0] * BC for i in range(ROUNDS + 1)]
-        ROUND_KEY_COUNT = (ROUNDS + 1) * BC
-        KC = int(len(key) / 4)
+        Kd = [[0] * 4 for i in range(11)]
 
         # copy user material bytes into temporary ints
         tk = []
-        for i in range(0, KC):
+        for i in range(0, 4):
             tk.append((ord(key[i * 4]) << 24) | (ord(key[i * 4 + 1]) << 16) |
                       (ord(key[i * 4 + 2]) << 8) | ord(key[i * 4 + 3]))
 
         # copy values into round key arrays
-        t = 0
-        j = 0
-        while j < KC and t < ROUND_KEY_COUNT:
-            Ke[int(t / BC)][t % BC] = tk[j]
-            Kd[ROUNDS - (int(t / BC))][t % BC] = tk[j]
+        t = j = tt = 0
+
+        while j < 4 and t < 44:
+            Ke[int(t / 4)][t % 4] = tk[j]
+            Kd[10 - (int(t / 4))][t % 4] = tk[j]
             j += 1
             t += 1
-        tt = 0
+
         rconpointer = 0
-        while t < ROUND_KEY_COUNT:
+        while t < 44:
             # extrapolate using phi (the round key evolution function)
-            tt = tk[KC - 1]
+            tt = tk[4 - 1]
             tk[0] ^= (S[(tt >> 16) & 0xFF] & 0xFF) << 24 ^  \
                      (S[(tt >> 8) & 0xFF] & 0xFF) << 16 ^  \
                      (S[tt & 0xFF] & 0xFF) << 8 ^  \
                      (S[(tt >> 24) & 0xFF] & 0xFF) ^  \
                      (rcon[rconpointer] & 0xFF) << 24
             rconpointer += 1
-            if KC != 8:
-                for i in range(1, KC):
-                    tk[i] ^= tk[i - 1]
-            else:
-                for i in range(1, int(KC / 2)):
-                    tk[i] ^= tk[i - 1]
-                tt = tk[int(KC / 2 - 1)]
-                tk[int(KC / 2)] ^= (S[tt & 0xFF] & 0xFF) ^ \
-                    (S[(tt >> 8) & 0xFF] & 0xFF) << 8 ^ \
-                    (S[(tt >> 16) & 0xFF] & 0xFF) << 16 ^ \
-                    (S[(tt >> 24) & 0xFF] & 0xFF) << 24
-                for i in range(int(KC / 2) + 1, KC):
-                    tk[i] ^= tk[i - 1]
+
+            for i in range(1, 4):
+                tk[i] ^= tk[i - 1]
+
             # copy values into round key arrays
             j = 0
-            while j < KC and t < ROUND_KEY_COUNT:
-                Ke[int(t / BC)][t % BC] = tk[j]
-                Kd[ROUNDS - (int(t / BC))][t % BC] = tk[j]
+            while j < 4 and t < 44:
+                Ke[int(t / 4)][t % 4] = tk[j]
+                Kd[10 - (int(t / 4))][t % 4] = tk[j]
                 j += 1
                 t += 1
         # inverse MixColumn where needed
-        for r in range(1, ROUNDS):
-            for j in range(BC):
-                tt = Kd[r][j]
-                Kd[r][j] = U1[(tt >> 24) & 0xFF] ^ \
-                    U2[(tt >> 16) & 0xFF] ^ \
-                    U3[(tt >> 8) & 0xFF] ^ \
-                    U4[tt & 0xFF]
+        for r in range(1, 10):
+            for i in range(4):
+                tt = Kd[r][i]
+                Kd[r][i] = U1[(tt >> 24) & 0xFF] ^ U2[(tt >> 16) & 0xFF] ^ \
+                    U3[(tt >> 8) & 0xFF] ^ U4[tt & 0xFF]
         self.Ke = Ke
         self.Kd = Kd
 
     def encrypt(self, plaintext):
-        if len(plaintext) != self.block_size:
-            raise ValueError('wrong block length, expected ' + str(self.block_size) + ' got ' + str(len(plaintext)))
+        if len(plaintext) != 16:
+            raise ValueError('wrong block length, expected {} got {}'.format(16, str(len(plaintext))))
         Ke = self.Ke
 
-        BC = 4
-        ROUNDS = 10
-        SC = 0
-
-        s1 = shifts[SC][1][0]
-        s2 = shifts[SC][2][0]
-        s3 = shifts[SC][3][0]
-        a = [0] * BC
+        s1 = shifts[1][0]
+        s2 = shifts[2][0]
+        s3 = shifts[3][0]
+        a = [0] * 4
         # temporary work array
-        t = []
+        t = [0] * 4
         # plaintext to ints + key
-        for i in range(BC):
-            t.append((ord(plaintext[i * 4]) << 24 |
-                      ord(plaintext[i * 4 + 1]) << 16 |
-                      ord(plaintext[i * 4 + 2]) << 8 |
-                      ord(plaintext[i * 4 + 3])) ^ Ke[0][i])
+        for i in range(4):
+            t[i] = (ord(plaintext[i * 4]) << 24 |
+                    ord(plaintext[i * 4 + 1]) << 16 |
+                    ord(plaintext[i * 4 + 2]) << 8 |
+                    ord(plaintext[i * 4 + 3])) ^ Ke[0][i]
         # apply round transforms
-        for r in range(1, ROUNDS):
-            for i in range(BC):
+        for r in range(1, 10):
+            for i in range(4):
                 a[i] = (T1[(t[i] >> 24) & 0xFF] ^
-                        T2[(t[(i + s1) % BC] >> 16) & 0xFF] ^
-                        T3[(t[(i + s2) % BC] >> 8) & 0xFF] ^
-                        T4[t[(i + s3) % BC] & 0xFF]) ^ Ke[r][i]
+                        T2[(t[(i + s1) % 4] >> 16) & 0xFF] ^
+                        T3[(t[(i + s2) % 4] >> 8) & 0xFF] ^
+                        T4[t[(i + s3) % 4] & 0xFF]) ^ Ke[r][i]
             t = copy.deepcopy(a)
         # last round is special
         result = []
-        for i in range(BC):
-            tt = Ke[ROUNDS][i]
+        for i in range(4):
+            tt = Ke[10][i]
             result.append((S[(t[i] >> 24) & 0xFF] ^ (tt >> 24)) & 0xFF)
-            result.append((S[(t[(i + s1) % BC] >> 16) & 0xFF] ^ (tt >> 16)) & 0xFF)
-            result.append((S[(t[(i + s2) % BC] >> 8) & 0xFF] ^ (tt >> 8)) & 0xFF)
-            result.append((S[t[(i + s3) % BC] & 0xFF] ^ tt) & 0xFF)
+            result.append((S[(t[(i + s1) % 4] >> 16) & 0xFF] ^ (tt >> 16)) & 0xFF)
+            result.append((S[(t[(i + s2) % 4] >> 8) & 0xFF] ^ (tt >> 8)) & 0xFF)
+            result.append((S[t[(i + s3) % 4] & 0xFF] ^ tt) & 0xFF)
         return ''.join(list(map(chr, result)))
 
     def decrypt(self, ciphertext):
-        if len(ciphertext) != self.block_size:
-            raise ValueError('wrong block length, expected ' + str(self.block_size) + ' got ' + str(len(ciphertext)))
+        if len(ciphertext) != 16:
+            raise ValueError('wrong block length, expected expected {} got {}'.format(16, str(len(plaintext))))
         Kd = self.Kd
 
-        BC = int(self.block_size / 4)
-        ROUNDS = len(Kd) - 1
-        if BC == 4:
-            SC = 0
-        elif BC == 6:
-            SC = 1
-        else:
-            SC = 2
-        s1 = shifts[SC][1][1]
-        s2 = shifts[SC][2][1]
-        s3 = shifts[SC][3][1]
-        a = [0] * BC
-        # temporary work array
-        t = [0] * BC
+        s1 = shifts[1][1]
+        s2 = shifts[2][1]
+        s3 = shifts[3][1]
+        a = [0] * 4
+        t = [0] * 4
+
         # ciphertext to ints + key
-        for i in range(BC):
+        for i in range(4):
             t[i] = (ord(ciphertext[i * 4]) << 24 |
                     ord(ciphertext[i * 4 + 1]) << 16 |
                     ord(ciphertext[i * 4 + 2]) << 8 |
                     ord(ciphertext[i * 4 + 3])) ^ Kd[0][i]
         # apply round transforms
-        for r in range(1, ROUNDS):
-            for i in range(BC):
+        for r in range(1, 10):
+            for i in range(4):
                 a[i] = (T5[(t[i] >> 24) & 0xFF] ^
-                        T6[(t[(i + s1) % BC] >> 16) & 0xFF] ^
-                        T7[(t[(i + s2) % BC] >> 8) & 0xFF] ^
-                        T8[t[(i + s3) % BC] & 0xFF]) ^ Kd[r][i]
+                        T6[(t[(i + s1) % 4] >> 16) & 0xFF] ^
+                        T7[(t[(i + s2) % 4] >> 8) & 0xFF] ^
+                        T8[t[(i + s3) % 4] & 0xFF]) ^ Kd[r][i]
             t = copy.deepcopy(a)
         # last round is special
         result = []
-        for i in range(BC):
-            tt = Kd[ROUNDS][i]
+        for i in range(4):
+            tt = Kd[10][i]
             result.append((Si[(t[i] >> 24) & 0xFF] ^ (tt >> 24)) & 0xFF)
-            result.append((Si[(t[(i + s1) % BC] >> 16) & 0xFF] ^ (tt >> 16)) & 0xFF)
-            result.append((Si[(t[(i + s2) % BC] >> 8) & 0xFF] ^ (tt >> 8)) & 0xFF)
-            result.append((Si[t[(i + s3) % BC] & 0xFF] ^ tt) & 0xFF)
+            result.append((Si[(t[(i + s1) % 4] >> 16) & 0xFF] ^ (tt >> 16)) & 0xFF)
+            result.append((Si[(t[(i + s2) % 4] >> 8) & 0xFF] ^ (tt >> 8)) & 0xFF)
+            result.append((Si[t[(i + s3) % 4] & 0xFF] ^ tt) & 0xFF)
         return ''.join(list(map(chr, result)))
 
 
-r = Rijndael("abcdef1234567890", block_size=16)
-ciphertext = r.encrypt("abcdef1234567890")
+r = Rijndael("Thiz_iz_AES_skey")
+ciphertext = r.encrypt("plaintxt2encrypt")
 plaintext = r.decrypt(ciphertext)
+print(plaintext)
+
 print(plaintext, ciphertext)
